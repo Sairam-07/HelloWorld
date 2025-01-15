ff6f55ab30a465cec8fc6b55f4fc2f2eb4afa1fb
Sairam-07 <pavandurgasairam24@gmail.com>
2025-01-15 12:34:53 +0530

    Changes done

--- js/hello.js
+++ js/hello.js
@@ -7,7 +7,7 @@ function HelloWorld(id) {
   greeting = document.createElement('h3');
   greeting.setAttribute('id', 'hello');
   greeting.style.visibility = "hidden";
-  greeting.innerHTML = "Hello World!I'm Sairam";
+  greeting.innerHTML = "Hello World!I'm Sairam from rjy";
 
   element.appendChild(greeting);
 
